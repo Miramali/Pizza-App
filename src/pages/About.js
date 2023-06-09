60aa1d5e1a3961b070e6daf02747f07b83e93af6
MiraMali <k.miira24@gmail.com>
2023-06-09 14:56:51 +0100

    change two

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,7 +8,7 @@ function About() {
         <div className='aboutTop' style={{ backgroundImage: `url(${aboutPic})`}}></div>
         <div className='aboutBottom'>
             <h1>About Us</h1>
-            <p>
+            <p >
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -16,19 +16,9 @@ function About() {
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
              containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
              including versions of Lorem Ipsum.
-             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-             containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-             including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            It has survived not only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-             containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
-             including versions of Lorem Ipsum.
+            
+             
+            
             </p>
         </div>
       
